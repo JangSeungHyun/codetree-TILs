4924bb5884f49e083fe56191c56541e0fc01fdec
Jang Seunghyun <jang1751@snu.ac.kr>
2024-10-10 00:42:24 +0900

    Title: 색깔 트리, Time: 9593ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241010/색깔 트리/color-tree.py
+++ 241010/색깔 트리/color-tree.py
@@ -17,22 +17,24 @@ class node:
 class tree:
 
     def __init__(self):
-        self.root = None
+        self.root = node(-1, -1, 0, 20000)
     
     # m_id로 node 찾아서 리턴하는 함수
     def find_node(self, m_id):
         
         queue = list()
-        queue.append(self.root)
+        for sub_root in self.root.child:
 
-        while len(queue) > 0:
-            curr_node = queue.pop(0)
+            queue.append(sub_root)
 
-            for node in curr_node.child:
-                queue.append(node)
+            while len(queue) > 0:
+                curr_node = queue.pop(0)
 
-            if curr_node.m_id == m_id:
-                return curr_node
+                for node in curr_node.child:
+                    queue.append(node)
+
+                if curr_node.m_id == m_id:
+                    return curr_node, sub_root
 
             
     def add_node(self, m_id, p_id, color, max_depth):
@@ -74,25 +76,24 @@ class tree:
                             return stack
                     
 
-        # max depth 고려해야 -> root로 추가되면 모든 노드 점검, 아니라면 본인부터 올라가며 점검
-        # 어떻게 parent node를 찾을까?
-        if p_id == -1:
-            if self.root == None: # 루트 새로 지정
-                self.root = node(m_id, p_id, color, max_depth)
-            else: # 루트 대체 필요 -> 새로운 루트의 max_depth와 height 비교필요
-                if find_height(self.root) + 1 > self.root.max_depth:
-                    return
-                temp = self.root
-                self.root = node(m_id, p_id, color, max_depth)
-                self.root.child.append(temp)
+        if p_id == -1: # 새로운 트리 생성 필요
+            self.root.child.append(node(m_id, p_id, color, max_depth))
+            # if self.root == None: # 루트 새로 지정
+            #     self.root =
+            # else: # 루트 대체 필요 -> 새로운 루트의 max_depth와 height 비교필요
+            #     if find_height(self.root) + 1 > self.root.max_depth:
+            #         return
+            #     temp = self.root
+            #     self.root = node(m_id, p_id, color, max_depth)
+            #     self.root.child.append(temp)
         else: # max depth
-            p_node = self.find_node(p_id)
+            p_node, sub_root = self.find_node(p_id)
             if len(p_node.child) != 0:
                 p_node.child.append(node(m_id, p_id, color, max_depth))
             else:
                 # 경로 stack 구성
                 stack = list()
-                stack = find_node_and_stack(self.root, stack, p_id)
+                stack = find_node_and_stack(sub_root, stack, p_id)
 
                 while len(stack) > 0:
                     curr_root = stack.pop(len(stack) - 1)
@@ -122,12 +123,12 @@ class tree:
             else:
                 return
 
-        curr_root = self.find_node(m_id)
+        curr_root, sub_root = self.find_node(m_id)
         change_sub_tree(curr_root, color)
 
 
     def find_color(self, m_id):
-        target_node = self.find_node(m_id)
+        target_node, sub_root = self.find_node(m_id)
         print(target_node.retrieve_color())
     
     
@@ -142,18 +143,20 @@ class tree:
                 
             return color_set
 
-        queue = list()
-        queue.append(self.root)
         total_value = 0
 
-        while len(queue) > 0:
-            curr_root = queue.pop(0)
-            for node in curr_root.child:
-                queue.append(node)
-            
-            color_set = set([])
-            num_color = len(retrieve_subtree_num_colors(curr_root, color_set))
-            total_value += num_color ** 2
+        for sub_root in self.root.child:
+            queue = list()
+            queue.append(sub_root)
+
+            while len(queue) > 0:
+                curr_root = queue.pop(0)
+                for node in curr_root.child:
+                    queue.append(node)
+                
+                color_set = set([])
+                num_color = len(retrieve_subtree_num_colors(curr_root, color_set))
+                total_value += num_color ** 2
 
         print(total_value)
 
